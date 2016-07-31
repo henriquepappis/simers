5016a6367121d5447130321ed63e88e67aa7893d
Henrique Pappis <contact@henriquepappis.com>
2016-07-31 12:21:12 -0300

    Acesso ao Banco de dados

--- model/model.php
+++ model/model.php
@@ -1,6 +1,37 @@
 	<?php
 	include_once("model/user.php");
 
+	class Database{
+		private static $dbName = 'simers' ;
+		private static $dbHost = 'localhost' ;
+		private static $dbUsername = 'root';
+		private static $dbUserPassword = '';
+
+		private static $cont  = null;
+
+		public function __construct(){
+			die('Função Init não permitida');
+		}
+
+		public static function connect(){
+			// Uma conexão para toda a aplicação
+			if ( null == self::$cont ){     
+				try{
+					self::$cont =  new PDO( "mysql:host=".self::$dbHost.";"."dbname=".self::$dbName, self::$dbUsername, self::$dbUserPassword);
+					self::$cont->exec("set names utf8"); 
+				}
+				catch(PDOException $e){
+					die($e->getMessage()); 
+				}
+			}
+			return self::$cont;
+		}
+
+		public static function disconnect(){
+			self::$cont = null;
+		}
+	}
+
 	class Model {
 		public function getUserListXML(){
 			// Leitura arquivo user.xml
@@ -21,6 +52,22 @@
 				}
 			}
 			return $listaUser;
+		}
+
+		public function getUserList(){
+			$pdo = Database::connect();
+			$sql = "SELECT * FROM user WHERE status=1 ORDER BY id";
+			foreach ($pdo->query($sql) as $row) {
+				$id = $row['id'];
+				$nome = $row['nome'];
+				$sobrenome = $row['sobrenome'];
+				$usuario = $row['usuario'];
+				$email = $row['email'];
+
+				$listaUser[] = new User($id, $nome, $sobrenome, $usuario, $email);
+			}
+			Database::disconnect();
+			return $listaUser;		
 		}
 	}
 ?>
